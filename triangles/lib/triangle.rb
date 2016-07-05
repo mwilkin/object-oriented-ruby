1f20c9794dceffed17639afb5402bef51a2baa54
Matt Wilkin and Noah Yasskin <noah@>
2016-07-05 09:58:54 -0700

    add a test for scalene and willestablish 4 passing examples.

--- triangles/lib/triangle.rb
+++ triangles/lib/triangle.rb
@@ -8,8 +8,18 @@ class Triangle
   define_method(:triangle?) do
     !(@side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side3 + @side1 <= @side2)
   end
-
+  
   define_method(:equilateral?) do
     @side1 == @side2 && @side2 == @side3
+  end
+
+  define_method(:isosceles?) do
+    (@side1 == @side2) && @side1 != @side3 ||
+    (@side2 == @side3) && @side2 != @side1 ||
+    (@side3 == @side1) && @side3 != @side2
+  end
+
+  define_method(:scalene?) do
+    @side1 != @side2 && @side2 != @side3 && @side1 != @side3
   end
 end
--- triangles/spec/triangle_spec.rb
+++ triangles/spec/triangle_spec.rb
@@ -12,5 +12,15 @@ describe(Triangle) do
       test_triangle = Triangle.new(4, 4, 4)
       expect(test_triangle.equilateral?()).to(eq(true))
     end
+
+    it('returns true if it is an isosceles triangle - only 2 equal sides') do
+      test_triangle = Triangle.new(4, 3, 4)
+      expect(test_triangle.isosceles?()).to(eq(true))
+    end
+
+    it('returns true if it is a scalene triangle - no sides are equal') do
+      test_triangle = Triangle.new(4, 3, 5)
+      expect(test_triangle.scalene?()).to(eq(true))
+    end
   end
 end
