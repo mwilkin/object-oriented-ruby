9892daf7eb8dbd888d91cc1d160d5be3f7bdd0f6
Matt Wilkin and Noah Yasskin <matt@>
2016-07-05 16:05:28 -0700

    add custom Class

---
+++ diy_hash/lib/myhash.rb
@@ -0,0 +1,23 @@
+class MyHash
+  define_method(:initialize) do
+    @my_hash = []
+  end
+
+  define_method(:my_hash) do
+    @my_hash
+  end
+
+  define_method(:myStore) do |key, value|
+    kv_array = [key,value]
+    @my_hash.push(kv_hash)
+  end
+
+  define_method(:myFetch) do |key|
+    @my_hash.each do |kv_array|
+      if kv_array[0] == key
+        return kv_array[1]
+      end
+    end
+  end
+
+end
---
+++ diy_hash/spec/myhash_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('my_hash')
+
+describe(MyHash) do
+  describe("#myFetch") do
+
+    it("retrieves a stored value by its key") do
+      test_hash = MyHash.new()
+      test_hash.myStore("kitten", "cute")
+      expect(test_hash.myFetch("kitten")).to(eq("cute"))
+    end
+
+  end
+end
